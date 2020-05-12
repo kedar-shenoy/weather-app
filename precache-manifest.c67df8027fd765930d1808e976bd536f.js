66cf3b709494862b50fc945689d13d9ba0f186d7
kedar-shenoy <kedarshenoy99@gmail.com>
2020-05-12 16:29:57 +0530

    Updates

---
+++ precache-manifest.c67df8027fd765930d1808e976bd536f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa0822e4c808e748ab235d76a039ced",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "10be37b1502d71c61641",
+    "url": "/weather-app/static/css/main.e521aeb9.chunk.css"
+  },
+  {
+    "revision": "1a9737ceca99d5c36159",
+    "url": "/weather-app/static/js/2.35d366ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.35d366ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10be37b1502d71c61641",
+    "url": "/weather-app/static/js/main.3f207991.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  },
+  {
+    "revision": "d82edfd8490cc29b01c0385ca8d191e5",
+    "url": "/weather-app/static/media/bg.d82edfd8.jpeg"
+  }
+]);
